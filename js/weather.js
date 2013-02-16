53e7c80050c5309d0cb08c996e985ce2cd054bd5
Dan Kim <git@dankim.org>
2013-02-15 21:40:50 -0600

    Change proxy.php location to different webserver

--- js/weather.js
+++ js/weather.js
@@ -39,7 +39,9 @@ $(document).ready(function() {
   }
 
   function queryWeatherData(queryEnd) {
-    $.getJSON('proxy.php?url=http://api.wunderground.com/api/593fb0bac5e11e4a/conditions/forecast10day/q/' + queryEnd + '.json', function(json) {
+    // $.getJSON('proxy.php?url=http://api.wunderground.com/api/593fb0bac5e11e4a/conditions/forecast10day/q/' + queryEnd + '.json', function(json) {
+          $.getJSON('http://dankim.org/proxy.php?url=http://api.wunderground.com/api/593fb0bac5e11e4a/conditions/forecast10day/q/' + queryEnd + '.json', function(json) {
+
       // Right now conditions
       $('#current-temp').html(json.contents.current_observation.temp_f + '&deg;');
       $('#current-location').html('in ' + json.contents.current_observation.display_location.full);
